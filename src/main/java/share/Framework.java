dd66ba4f43a2ae9e148c013e4edf32db614ff1d8
teamlead11 <automationlead11@gmail.com>
2021-01-18 11:31:26 +0530

    delete sample code

--- src/main/java/share/Framework.java
+++ src/main/java/share/Framework.java
@@ -5,8 +5,8 @@ public class Framework {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("this is my framework");
-		System.out.println("adding a new line");
+		System.out.println("this is my new framework");
+		System.out.println("new line");
 	}
 
 }
