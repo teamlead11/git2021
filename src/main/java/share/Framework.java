6a7331e8715b663a8244031250d2b3ab14bf005e
teamlead11 <automationlead11@gmail.com>
2021-01-18 11:17:54 +0530

    added initial framework components

---
+++ src/main/java/share/Framework.java
@@ -0,0 +1,11 @@
+package share;
+
+public class Framework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is my framework");
+	}
+
+}
