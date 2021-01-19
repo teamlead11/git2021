606313911aa5e2b291d00c72ce57a14647909481
karthi1306 <skarthick0113@gmail.com>
2021-01-19 11:08:46 +0530

    modified test case1

--- src/main/java/share/TestCase1.java
+++ src/main/java/share/TestCase1.java
@@ -5,6 +5,11 @@ public class TestCase1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is my test case1");
+		TestCase1 obj = new TestCase1();
+		obj.add();
 	}
 
+	public void add() {
+		System.out.println("Hi");
+	}
 }
